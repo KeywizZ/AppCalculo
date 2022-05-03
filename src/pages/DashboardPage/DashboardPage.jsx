5f37cad57fb47401deff41177ef0fef6ca7aedd0
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-03 15:21:21 +0200

    link dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,6 +4,7 @@ import { ActivityList } from '../../components/ActivityList/ActivityList'
 export const DashboardPage = () => {
   return (
     <div className='dashboard'>
+        <div>Hola desde Dashboard</div>
         <ActivityList />
     </div>
   )
