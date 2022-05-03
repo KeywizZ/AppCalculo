e2701a3132ff9bdac3a5d8fb32542f2e953b5e1a
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-03 15:54:57 +0200

    git conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,8 @@ import {
   Routes,
 } from "react-router-dom";
 import { useState } from "react";
-<<<<<<< HEAD
 import { HomePage } from "./pages/HomePage/HomePage";
 import { GiHamburgerMenu } from "react-icons/gi";
-
-function App() {
-  /*   const [jwt, setJwt] = useState(localStorage.getItem("token")); */
-
-  return (
-    <div className="App">
-      <nav className="Nav">
-        <h1>AppCálculo</h1>
-        <button><GiHamburgerMenu /></button>
-      </nav>
-      <Router>
-        <Routes>
-          <Route path="/">
-            <Route index element={<HomePage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </div>
-=======
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
 import { JwtContext } from "./shared/context/JwtContext";
@@ -46,7 +26,6 @@ function App() {
         </Router>
       </div>
     </JwtContext.Provider>
->>>>>>> xulio
   );
 }
 
