e07e360ea05a208768ba3e313db88f1dfa28b5ec
KeywizZ <0keywiz0@gmail.com>
2022-05-03 15:11:08 +0200

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,9 @@ import {
   Routes,
 } from "react-router-dom";
 import { useState } from "react";
-<<<<<<< HEAD
-import { ActivityList } from "./components/ActivityList/ActivityList";
-
-
-
-
-
-function App() {
-//const [jwt, setJwt] = useState(localStorage.getItem("token"));
-  
-  return <div className="App">
-  </div>;
-=======
 import { HomePage } from "./pages/HomePage/HomePage";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { ActivityList } from "./components/ActivityList/ActivityList";
 
 function App() {
   /*   const [jwt, setJwt] = useState(localStorage.getItem("token")); */
@@ -29,7 +17,9 @@ function App() {
     <div className="App">
       <nav className="Nav">
         <h1>AppCálculo</h1>
-        <button><GiHamburgerMenu /></button>
+        <button>
+          <GiHamburgerMenu />
+        </button>
       </nav>
       <Router>
         <Routes>
@@ -40,7 +30,6 @@ function App() {
       </Router>
     </div>
   );
->>>>>>> f44ad596b35eb0f5292cb7cb54141cb58cd4d75d
 }
 
 export default App;
