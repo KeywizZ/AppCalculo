441c661de0977fc9b62d96ecf0536f48f2aab4f8
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-04 23:36:40 +0200

    activities by type component & hide button ok

--- src/components/ActivityList/ActivityList.jsx
+++ src/components/ActivityList/ActivityList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { ActivityByType } from "./ActivityByType";
 
 export const ActivityList = () => {
   const [activities, setActivities] = useState([]);
@@ -8,50 +8,20 @@ export const ActivityList = () => {
 
   useEffect(() => {
     axios(url).then((res) => {
-      console.log("INFO: Getting data from API");
+      console.log("INFO: (ActivityList) Getting data from API");
       setActivities(res.data);
     });
-  }, [url]);
+  }, []);
 
   const types = [...new Set(activities.map((item) => item.type))];
 
-  const setVisivility = (id) => {
-    try {
-      if (document.getElementById(id).style.visibility === "show") document.querySelector(id).style.visibility = "hidden";
-      else document.querySelector(id).style.visibility = "show";
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
-    <>
-      <div className="container-activities">
-        <h2>Lista de actividades</h2>
-        {activities &&
-          types.map((item) => {
-            return (
-              <>
-                <button className="list-activity-button" onClick={document.getElementById({item}).hidden = !document.getElementById({item}).hidden}>
-                  {item}
-                </button>
-                <div key={JSON.stringify(item)} className="activities" id={item}>
-                  {activities
-                    .filter((activity) => activity.type.includes(item))
-                    .map((activity) => {
-                      return (
-                        <div className="activity-card" key={JSON.stringify(activity)}>
-                          <Link to={`activities/${activity._id}`} className="activity-anchor">
-                            <button className="activity-btn">{activity.id}</button>
-                          </Link>
-                        </div>
-                      );
-                    })}
-                </div>
-              </>
-            );
-          })}
-      </div>
-    </>
+    <div className="container-activities">
+      <h2>Lista de actividades</h2>
+      {activities &&
+        types.map((type) => {
+          return <ActivityByType type={type} />;
+        })}
+    </div>
   );
 };
