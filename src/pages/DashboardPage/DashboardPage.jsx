31db0ae696985c73921f565befe078b186b9c737
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-05-04 13:02:18 +0200

    Role dashboard manager

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -2,9 +2,10 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ActivityList } from "../../components/ActivityList/ActivityList";
-const user = JSON.parse(localStorage.getItem("user"));
 
 export const DashboardPage = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user.role);
   return (
     <div className="dashboard">
       {user.role === "STUDENT" && <ActivityList />}
