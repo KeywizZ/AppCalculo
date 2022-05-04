a74ea0fb1cd2ba327425006fa63e65f675cec5f3
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-05-04 12:50:18 +0200

    roles dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -2,15 +2,17 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ActivityList } from "../../components/ActivityList/ActivityList";
+const user = JSON.parse(localStorage.getItem("user"));
 
 export const DashboardPage = () => {
   return (
     <div className="dashboard">
-      <ActivityList />
-      <NavLink to="/register">
-        <button className="login">Registrar un nuevo usuario</button>
-      </NavLink>
+      {user.role === "STUDENT" && <ActivityList />}
+      {user.role === "ADMIN" && (
+        <NavLink to="/register">
+          <button className="login">Registrar un nuevo usuario</button>
+        </NavLink>
+      )}
     </div>
-    
   );
 };
