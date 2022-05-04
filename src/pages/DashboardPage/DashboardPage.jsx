747edcba07f8b0301824040f44e93b9d69c48926
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-05-04 12:15:45 +0200

    Register button dashboard

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,11 +1,16 @@
 //import { useAuth0 } from '@auth0/auth0-react'
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { ActivityList } from "../../components/ActivityList/ActivityList";
 
 export const DashboardPage = () => {
   return (
     <div className="dashboard">
       <ActivityList />
+      <NavLink to="/register">
+        <button className="login">Registrar un nuevo usuario</button>
+      </NavLink>
     </div>
+    
   );
 };
