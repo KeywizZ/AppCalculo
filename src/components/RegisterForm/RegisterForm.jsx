45580e9d3ce84d51f835e482fdce53d3b4261b14
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-05-05 15:59:55 +0200

    form connects to api

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -16,25 +16,16 @@ export const RegisterForm = () => {
 
   const onSubmit = (formData, e) => {
     e.preventDefault();
-    /*    console.log(e.target.elements.name.value);
-    console.log(e.target.name.value) ; */
 
-    emailjs
-      .sendForm(
-        "service_q51kznk",
-        "template_2wp305r",
-        form.current,
-        "JtsjEFNXDwJJYbZ46"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("Usuario registrado con exito");
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm("service_q51kznk", "template_2wp305r", form.current, "JtsjEFNXDwJJYbZ46").then(
+      (result) => {
+        console.log(result.text);
+        alert("Usuario registrado con exito");
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
     API.post("users/register", formData)
       .then((res) => {
         console.log(res);
@@ -45,50 +36,68 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form
-      className="container-form"
-      ref={form}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="user-input">
+    <form className="container-form" ref={form} onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-input">
         <label>Nombre de Usuario: </label>
-        <input
-          type="text"
-          name="user_name"
-          {...register("user_name", { required: true })}
-        />
+        <input type="text" name="user_name" {...register("name", { required: true })} />
       </div>
-      <div className="pass-input">
+      <div className="form-input">
         <label>Correo electrónico: </label>
         <input
           type="email"
           name="user_email"
-          {...register("user_email", {
+          {...register("email", {
             required: true,
             pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
           })}
         />
+      </div>
+      <div className="form-input">
         <label>Contraseña: </label>
         <input
           type="password"
           name="user_password"
-          {...register("user_password", {
+          {...register("password", {
             required: true,
-            pattern:
-              /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
+            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
           })}
         />
       </div>
-      <div className="form-role">
+      <div className="form-input">
+        <label>Nombre Tutor/a: </label>
+        <input type="text" name="guardian_name" {...register("guardianName")} />
+      </div>
+      <div className="form-input">
+        <label>Correo electrónico Tutor/a: </label>
+        <input
+          type="email"
+          name="guardian_email"
+          {...register("guardianEmail", {
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+          })}
+        />
+      </div>
+      <div className="form-drop">
         <label>Rol: </label>
         <select {...register("role", { require: true })}>
           <option>ADMIN</option>
           <option>STUDENT</option>
-          <option>GUARDIANS</option>
           <option>TEACHER</option>
         </select>
-        <button className="btn">Registro</button>
       </div>
+      <div className="form-drop">
+        <label>Grupo / Aula: </label>
+        <select {...register("group", { require: true })}>
+          <option>ADMINISTRATIVO</option>
+          <option>1EP</option>
+          <option>2EP</option>
+          <option>3EP</option>
+          <option>4EP</option>
+          <option>5EP</option>
+          <option>6EP</option>
+        </select>
+      </div>
+      <button className="btn">Registro</button>
     </form>
   );
 };
