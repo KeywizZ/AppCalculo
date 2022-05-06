1e1e6671badd60c51227adaf9a2069594894aca9
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-05-06 15:04:50 +0200

    Fixed some auths

--- src/shared/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/shared/components/HamburgerMenu/HamburgerMenu.jsx
@@ -4,10 +4,12 @@ import { ImCross } from "react-icons/im";
 //import { useNavigate } from "react-router-dom";
 import { ButtonLogout } from "../../../components/LoginForm/ButtonLogout";
 import { JwtContext } from "../../context/JwtContext";
+ 
 
 export const HamburgerMenu = () => {
   const { jwt } = useContext(JwtContext);
 
+  const user = JSON.parse(sessionStorage.getItem("user"));
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
   const handleToggle = () => {
     setHamburgerOpen(!hamburgerOpen);
@@ -22,11 +24,20 @@ export const HamburgerMenu = () => {
       </div>
       <ul
         id="nav-list"
-        className={`menuHamb ${hamburgerOpen ? " showMenu animate__animated animate__fadeInDown" : " hideMenu animate__animated animate__fadeOutUp"}`}
+        className={`menuHamb ${
+          hamburgerOpen
+            ? " showMenu animate__animated animate__fadeInDown"
+            : " hideMenu animate__animated animate__fadeOutUp"
+        }`}
       >
         <li>
           <a href="/">Home</a>
         </li>
+        {(jwt && user.role === "ADMIN") && (
+          <li>
+            <a href="/register">Registrar usuario</a>
+          </li>
+        )}
         <li>
           <a href="/about">Sobre Nosotros</a>
         </li>
@@ -38,11 +49,7 @@ export const HamburgerMenu = () => {
             <a href="/dashboard">Dashboard</a>
           </li>
         )}
-        {jwt && (
-          <li>
-            <a href="/register">Registrar usuario</a>
-          </li>
-        )}
+        
 
         {jwt && (
           <li className="logoutLi">
